70a62dd7213cf4aaab6fd67cccaee9c999fde36b
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-06-22 18:06:14 +0530

    Updated mongo client

--- src/clients/mongo.client.ts
+++ src/clients/mongo.client.ts
@@ -1,11 +1,9 @@
 import { FastifyInstance } from "fastify";
 import mongoose from 'mongoose';
-import dbModels, { DBModels } from "../models/index";
+import models, { DBModels } from "../models/index";
 import { logger } from "../common/services/logger.service";
 
 export namespace MongoClient {
-  export let models: DBModels = {}; // Declare a models object to hold all MongoDB models
-
   export async function init(fastify: FastifyInstance) {
     const mongoConnectionString = fastify.config["SERVER_MONGO_CONN"];
 
@@ -14,13 +12,6 @@ export namespace MongoClient {
     }).catch((error) => {
       fastify.log.error('Unable to connect to MongoDB:' + error.message);
       throw error;
-    });
-
-    // Iterate over Sequelize models and create corresponding Mongoose models
-    Object.keys(dbModels).forEach((dbModel) => {
-      console.log(`MODEL:${dbModel}`)
-      const mongooseSchema = new mongoose.Schema(dbModels[dbModel]);
-      models[dbModel] = mongoose.model(dbModel, mongooseSchema);
     });
 
     // Optionally, save the models object into Fastify instance for easy access
