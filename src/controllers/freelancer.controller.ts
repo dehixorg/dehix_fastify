5d640b03827cb09f92d6aa18d011a04ff9cffff7
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-06-24 22:22:31 +0530

    update project adde

--- src/controllers/freelancer.controller.ts
+++ src/controllers/freelancer.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { FastifyRequest, FastifyReply } from "fastify";
-import { Controller, GET, Inject, POST } from "fastify-decorators";
+import { Controller, DELETE, GET, Inject, POST, PUT } from "fastify-decorators";
 import { FreelancerService } from "../services";
 import {
   STATUS_CODES,
@@ -13,14 +13,16 @@ import {
   FREELANCER_ID_ENDPOINT,
   CREATE_PROJECT,
   FREELANCER_INFO,
-  FREELANCER_CREATE_ENDPOINT
+  FREELANCER_PROJECT_DELETE_BY_ID,
 } from "../constants/freelancer.constant";
 import { getFreelancerSchema } from "../schema/v1/freelancer/get";
 import { UnAuthorisedError } from "../common/errors";
 import { AuthController } from "../common/auth.controller";
-import {CreateFreelancerBody} from "../types/v1/freelancer/create"
-import { IFreelancer } from "src/models/freelancer.entity";
-import { createFreelancerSchema } from "../schema/v1/freelancer/create";
+import { addFreelancerProjectSchema, updateFreelancerSchema } from "src/schema/v1/freelancer/update";
+import { PutFreelancerPathParams, PutFreelancerBody } from "src/types/v1/freelancer/updateProfile";
+import { deleteFreelancerProjectSchema } from "src/schema/v1/freelancer/delete";
+import { DeleteFreelancerPathParams } from "src/types/v1/freelancer/delete";
+import { PutFreelancerProjectBody } from "src/types/v1/freelancer/updateProject";
 
 @Controller({ route: FREELANCER_ENDPOINT })
 export default class FreelancerController extends AuthController {
@@ -50,20 +52,56 @@ export default class FreelancerController extends AuthController {
       data,
     });
   }
-  @POST(FREELANCER_CREATE_ENDPOINT, { schema: createFreelancerSchema })
-  async create(
-    request: FastifyRequest<{ Body: IFreelancer }>,
-    reply: FastifyReply,
-  ) {
-    this.logger.info(
-      `FreelancerController -> create -> : Creating a new freelancer`,
-    );
-    const data = await this.freelancerService.createFreelancerProfile(
-      request.body,
-    );
-    reply.status(STATUS_CODES.SUCCESS).send({
-      data,
-    });
-  }
 
+  @PUT(FREELANCER_ID_ENDPOINT,{schema:updateFreelancerSchema})
+  async getByID(   request: FastifyRequest<{ Params: PutFreelancerPathParams , Body: PutFreelancerBody}>,
+    reply: FastifyReply,){
+      this.logger.info(
+        `FreelancerController -> getById -> Fetching vendor profile for vendor with ID: ${request.params.freelancer_id}`,
+      );
+      // if (request.decodedToken.userId !== request.params.freelancer_id) {
+    //   this.logger.error(
+    //     `FreelancerController -> getById -> Unauthorized access attempt by user with ID: ${request.decodedToken.userId} to vendor with ID: ${request.params.freelancer_id}`,
+    //   );
+    //   throw new UnAuthorisedError(RESPONSE_MESSAGE.UNAUTHORISED, ERROR_CODES.UNAUTHORIZED);
+    // }
+const data= await this.freelancerService.updateProfileFreelancer(request.params.freelancer_id,request.body.update)
+reply.status(STATUS_CODES.SUCCESS).send({data})
+    }
+
+    @DELETE(FREELANCER_PROJECT_DELETE_BY_ID,{schema:deleteFreelancerProjectSchema})
+    async deleteProjectById(   request: FastifyRequest<{ Params:DeleteFreelancerPathParams}>,
+      reply: FastifyReply,){
+        this.logger.info(
+          `FreelancerController -> getById -> Fetching vendor profile for vendor with ID: ${request.params.freelancer_id}`,
+        );
+        // if (request.decodedToken.userId !== request.params.freelancer_id) {
+      //   this.logger.error(
+      //     `FreelancerController -> getById -> Unauthorized access attempt by user with ID: ${request.decodedToken.userId} to vendor with ID: ${request.params.freelancer_id}`,
+      //   );
+      //   throw new UnAuthorisedError(RESPONSE_MESSAGE.UNAUTHORISED, ERROR_CODES.UNAUTHORIZED);
+      // }
+
+      const data= await this.freelancerService.deleteFreelancerProject(request.params.freelancer_id,request.params.project_id)
+reply.status(STATUS_CODES.SUCCESS).send({data})
+      }
+      @PUT(FREELANCER_ENDPOINT,{schema:addFreelancerProjectSchema})
+      async addProjectById(   request: FastifyRequest<{ Params:PutFreelancerPathParams,Body:PutFreelancerProjectBody}>,
+        reply: FastifyReply,){
+          this.logger.info(
+            `FreelancerController -> getById -> Fetching vendor profile for vendor with ID: ${request.params.freelancer_id}`,
+          );
+          // if (request.decodedToken.userId !== request.params.freelancer_id) {
+        //   this.logger.error(
+        //     `FreelancerController -> getById -> Unauthorized access attempt by user with ID: ${request.decodedToken.userId} to vendor with ID: ${request.params.freelancer_id}`,
+        //   );
+        //   throw new UnAuthorisedError(RESPONSE_MESSAGE.UNAUTHORISED, ERROR_CODES.UNAUTHORIZED);
+        // }
+  
+        const data= await this.freelancerService.addFreelancerProject(request.params.freelancer_id,request.body.update)
+  reply.status(STATUS_CODES.SUCCESS).send({data})
+        }
 }
+
+
+
