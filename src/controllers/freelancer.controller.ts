42742840d7fba22a6941b0267f3d52648d21834b
tushar2708 <rtushar2708@gmail.com>
2024-06-26 20:16:01 +0530

    Complete oracle status api

--- src/controllers/freelancer.controller.ts
+++ src/controllers/freelancer.controller.ts
@@ -16,17 +16,20 @@ import {
   FREELANCER_PROJECT_DELETE_BY_ID,
   FREELANCER_SKILLS_ADD_BY_ID,
   FREELANCER_SKILL_DELETE_BY_ID,
+  FREELANCER_ORACLE_STATUS_BY_ID,
 } from "../constants/freelancer.constant";
 import { getFreelancerSchema } from "../schema/v1/freelancer/get";
 import { AuthController } from "../common/auth.controller";
 import {
   addFreelancerProjectSchema,
+  oracleStatusSchema,
   updateFreelancerSchema,
 } from "../schema/v1/freelancer/update";
 import {
   PutFreelancerPathParams,
   PutFreelancerBody,
   PutFreelancerSkillsBody,
+  PutFreelancerOracleStatusBody,
 } from "../types/v1/freelancer/updateProfile";
 import {
   deleteFreelancerProjectSchema,
@@ -248,6 +251,34 @@ export default class FreelancerController extends AuthController {
       reply.status(STATUS_CODES.SUCCESS).send({ data });
     } catch (error: any) {
       this.logger.error(`Error in create: ${error.message}`);
+      reply.status(STATUS_CODES.SERVER_ERROR).send({
+        message: RESPONSE_MESSAGE.SERVER_ERROR,
+        code: ERROR_CODES.SERVER_ERROR,
+      });
+    }
+  }
+
+  @PUT(FREELANCER_ORACLE_STATUS_BY_ID, { schema: oracleStatusSchema })
+  async updateOracleStatusById(
+    request: FastifyRequest<{
+      Params: PutFreelancerPathParams;
+      Body: PutFreelancerOracleStatusBody;
+    }>,
+    reply: FastifyReply,
+  ) {
+    try {
+      this.logger.info(
+        `FreelancerController -> updateOracleStatusById -> Updating oracle status of freelancer using ID: ${request.params.freelancer_id}`,
+      );
+
+      const data = await this.freelancerService.updateFreelancerOracleStatus(
+        request.params.freelancer_id,
+        request.body.oracleStatus,
+      );
+
+      reply.status(STATUS_CODES.SUCCESS).send({ data });
+    } catch (error: any) {
+      this.logger.error(`Error in updateOracleStatusById: ${error.message}`);
       reply.status(STATUS_CODES.SERVER_ERROR).send({
         message: RESPONSE_MESSAGE.SERVER_ERROR,
         code: ERROR_CODES.SERVER_ERROR,
