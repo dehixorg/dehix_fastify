416a5664cfdc4c021e87d06a5944c173d0bf1db0
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-07-16 12:05:31 +0530

    added deployment files

--- src/common/services/firebase.service.ts
+++ src/common/services/firebase.service.ts
@@ -19,7 +19,10 @@ class FirebaseClient {
   async init() {
     const dirName = path.dirname(new URL(import.meta.url).pathname);
     const serviceAccountPath = path
-      .join(dirName, `../../../config/firebase-${process.env.NODE_ENV}.json`)
+      .join(
+        dirName,
+        `../../common/config/firebase-${process.env.NODE_ENV}.json`,
+      )
       .replace(/^\\([A-Za-z]:)/, "$1");
     const serviceAccount = JSON.parse(
       fs.readFileSync(serviceAccountPath, "utf8"),
