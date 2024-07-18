1146a20ef06525c718ffcb7b7d8d212fb2fbb74b
tushar2708 <rtushar2708@gmail.com>
2024-07-18 22:39:05 +0530

    api for get project by business id and status

--- src/schema/v1/business/get.ts
+++ src/schema/v1/business/get.ts
@@ -34,3 +34,48 @@ export const getBusinessSchema: FastifySchema = {
     },
   },
 };
+
+export const getBusinessProjectSchema: FastifySchema = {
+  description: "API to get BUSINESS project data",
+  tags: ["Project"],
+  querystring: {
+    type: "object",
+    properties: {
+      status: {
+        type: "string",
+        enum: ["Active", "Pending", "Completed", "Rejected"],
+        description: "Filter projects by status",
+      },
+    },
+  },
+  response: {
+    404: {
+      type: "object",
+      properties: {
+        message: {
+          type: "string",
+        },
+        code: {
+          type: "string",
+        },
+      },
+    },
+    403: {
+      type: "object",
+      properties: {
+        code: {
+          type: "string",
+        },
+        message: {
+          type: "string",
+        },
+      },
+    },
+    500: {
+      type: "object",
+      properties: {
+        message: { type: "string" },
+      },
+    },
+  },
+};
