309a7844b2f6f8fab4d3a99f1081ef3ea0a5605e
tushar2708 <rtushar2708@gmail.com>
2024-07-19 23:36:50 +0530

    fix bid constant

--- src/constants/bid.constant.ts
+++ src/constants/bid.constant.ts
@@ -3,5 +3,5 @@ export const BID_ID_ENDPOINT = "/createBid";
 export const UPDATE_BID_BY_ID_ENDPOINT = "/:bid_id";
 export const UPDATE_BID_STATUS_BY_ID_ENDPOINT = "/:bid_id/status";
 export const BID_ID_BUSINESS_END_POINT = "/:project_id/bids";
-export const BID_ID_FREELANCER_END_POINT = "/:bidder_id/bids";
+export const BID_ID_FREELANCER_END_POINT = "/:bidder_id/bid";
 export const DELETE_BID_END_POINT = "/:bid_id/delete";
