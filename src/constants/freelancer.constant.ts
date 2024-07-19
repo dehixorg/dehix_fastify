0ac7d68d1b2573919277d8ca6da54f79e44f0e8c
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-07-19 22:44:15 +0530

    fixes

--- src/constants/freelancer.constant.ts
+++ src/constants/freelancer.constant.ts
@@ -5,18 +5,24 @@ export const FREELANCER_ID_ENDPOINT = "/:freelancer_id";
 // project endpoint
 export const FREELANCER_PROJECT_ID_ENDPOINT = "/:freelancer_id/project";
 export const FREELANCER_PROJECT_ADD_BY_ID = "/:freelancer_id/project";
-export const FREELANCER_UPDATE_PROJECT_BY_ID = "/:freelancer_id/project/:project_id";
-export const FREELANCER_PROJECT_DELETE_BY_ID = "/:freelancer_id/project/:project_id";
+export const FREELANCER_UPDATE_PROJECT_BY_ID =
+  "/:freelancer_id/project/:project_id";
+export const FREELANCER_PROJECT_DELETE_BY_ID =
+  "/:freelancer_id/project/:project_id";
 
 // experience endpoint
 export const FREELANCER_CREATE_EXPERIENCE_BY_ID = "/:freelancer_id/experience";
-export const FREELANCER_UPDATE_EXPERIENCE_BY_ID = "/:freelancer_id/experience/:experience_id";
-export const FREELANCER_EXPERINCE_DELETE_BY_ID = "/:freelancer_id/experience/:experience_id";
+export const FREELANCER_UPDATE_EXPERIENCE_BY_ID =
+  "/:freelancer_id/experience/:experience_id";
+export const FREELANCER_EXPERINCE_DELETE_BY_ID =
+  "/:freelancer_id/experience/:experience_id";
 
 // education endpoint
 export const FREELANCER_CREATE_EDUCATION_BY_ID = "/:freelancer_id/education";
-export const FREELANCER_UPDATE_EDUCATION_BY_ID = "/:freelancer_id/education/:education_id";
-export const FREELANCER_DELETE_EDUCATION_BY_ID = "/:freelancer_id/education/:education_id";
+export const FREELANCER_UPDATE_EDUCATION_BY_ID =
+  "/:freelancer_id/education/:education_id";
+export const FREELANCER_DELETE_EDUCATION_BY_ID =
+  "/:freelancer_id/education/:education_id";
 
 // skills endpoint
 export const FREELANCER_SKILLS_ADD_BY_ID = "/:freelancer_id/skill";
@@ -36,4 +42,5 @@ export const SUBMIT_FOR_REVIEW_ENDPOINT = "/submit-for-review";
 export const CREATE_PROJECT = "/create_project"; //not used
 export const FREELANCER_INFO = "/freelancer_info";
 export const VERIFICATION_DOMAIN = "https://dev.findmyvenue.com/verify";
-export const RESET_PASSWORD_DOMAIN = "https://dev.findmyvenue.com/reset-password";
+export const RESET_PASSWORD_DOMAIN =
+  "https://dev.findmyvenue.com/reset-password";
