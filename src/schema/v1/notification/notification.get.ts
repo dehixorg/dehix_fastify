0c6a50ff8ac2bcddd11d807c43af79c233c6a10a
Uttam Gohil <itsjupiter000@gmail.com>
2024-09-08 23:24:26 +0530

    Error solved: removed 200 status response from getNotificationSchema

--- src/schema/v1/notification/notification.get.ts
+++ src/schema/v1/notification/notification.get.ts
@@ -4,36 +4,6 @@ export const getNotificationSchema: FastifySchema = {
   description: "API to get a Notification by ID",
   tags: ["Notification"],
   response: {
-    200: {
-      description: "Success",
-      type: "object",
-      properties: {
-        data: {
-          type: "array",
-          items: {
-            type: "object",
-            properties: {
-              _id: { type: "string" },
-              heading: { type: "string" },
-              description: { type: "string" },
-              type: { type: "string" },
-              status: { type: "string" },
-              background_img: { type: "string" },
-              importantUrl: {
-                type: "array",
-                items: {
-                  type: "object",
-                  properties: {
-                    urlName: { type: "string" },
-                    url: { type: "string" },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
     401: {
       type: "object",
       properties: {
