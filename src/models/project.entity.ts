016a1b18448709ec873624c3410b7dc902d6b349
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-09-15 10:09:36 +0530

    minor change

--- src/models/project.entity.ts
+++ src/models/project.entity.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
 import { v4 as uuidv4 } from "uuid";
-import { string } from "zod";
+
 
 // Define an interface for the Project document
 export interface IProject extends Document {
