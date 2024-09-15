69ad336dc85303e5f63ef25c6ae64b72501aeafc
Harshit Sharma <harshit.sharma8532@gmail.com>
2024-09-15 17:10:55 +0530

    Make interview workflow complete and test in end to end

--- src/constants/interview.constant.ts
+++ src/constants/interview.constant.ts
@@ -1,4 +1,9 @@
 export const INTERVIEW = "/interview";
-export const CREATE_INTERVIEW_END_POINT = "/:interviewee";
+export const CREATE_INTERVIEW_END_POINT = "/:_id";
 export const UPDATE_INTERVIEW_END_POINT = "/:interview_id";
 export const GET_ALL_INTERVIEW = "/all_interview";
+
+export const CompletedInterviewForInterviewee =
+  "/:interviewee_id/completedinterview";
+export const CurrentInterviewForInterviewee =
+  "/:interviewee_id/currentinterview";
