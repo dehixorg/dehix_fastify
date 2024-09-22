90b6ec3de1d400108171414e2d033d52fffa9839
harshitsharma7017 <138382940+harshitsharma7017@users.noreply.github.com>
2024-09-22 20:11:03 +0530

    changed /:id to /:interviewee_id

--- src/constants/interview.constant.ts
+++ src/constants/interview.constant.ts
@@ -1,5 +1,5 @@
 export const INTERVIEW = "/interview";
-export const CREATE_INTERVIEW_END_POINT = "/:_id";
+export const CREATE_INTERVIEW_END_POINT = "/:interviewee_id";
 export const UPDATE_INTERVIEW_END_POINT = "/:interview_id";
 export const GET_ALL_INTERVIEW = "/all_interview";
 
