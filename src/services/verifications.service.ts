928ae945b7c1383eaca22baeb1c0cebd1f1f4241
Harshit Sharma <harshit.sharma8532@gmail.com>
2024-10-13 11:49:55 +0530

    resoleved error in getting verification data for undefined value

--- src/services/verifications.service.ts
+++ src/services/verifications.service.ts
@@ -352,7 +352,7 @@ export class VerificationService extends BaseService {
 
   // Refactor the getAllVerificationData method in VerificationsService
   async getAllVerificationData(
-    doc_type:
+    doc_type?:
       | "skill"
       | "domain"
       | "education"
@@ -381,6 +381,10 @@ export class VerificationService extends BaseService {
       business: this.BusinessDAO.getBusinessById.bind(this.BusinessDAO),
     };
 
+    // If doc_type is undefined, return all data or handle the case accordingly
+    if (!doc_type) {
+      return data; // Return all verification data if doc_type is undefined
+    }
     if (!daoMethods[doc_type]) {
       throw new Error(`Unsupported doc_type: ${doc_type}`);
     }
