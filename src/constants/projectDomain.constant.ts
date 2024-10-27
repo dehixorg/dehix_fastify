dcd65255923c69eb501e68f29d3e1733d3389965
Uttam Gohil <itsjupiter000@gmail.com>
2024-10-27 16:27:51 +0530

    fixed project domain endpoints

--- src/constants/projectDomain.constant.ts
+++ src/constants/projectDomain.constant.ts
@@ -1,14 +1,6 @@
 // Base endpoint for project domain-related operations
-export const PROJECT_DOMAIN_ENDPOINT = "/projectDomain";
-
-// Endpoint for accessing a specific project domain (root endpoint)
-export const PROJECT_DOMAIN_ID_ENDPOINT = "/";
+export const PROJECT_DOMAIN_ENDPOINT = "/projectdomain";
 
 // Endpoint for deleting a project domain by its ID
-export const DELETE_PROJECT_DOMAIN_BY_ID_ENDPOINT = "/:projectDomain_id";
+export const PROJECT_DOMAIN_BY_ID_ENDPOINT = "/:projectdomain_id";
 
-// Endpoint for fetching all project domains
-export const PROJECT_DOMAIN_ALL_ENDPOINT = "/all";
-
-// Endpoint for updating a project domain by its ID
-export const UPDATE_PROJECT_DOMAIN_BY_ID_ENDPOINT = "/:projectDomain_id";
