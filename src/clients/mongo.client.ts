01924bf3e9a7a49c1cc524c81f8db59081f1df69
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-11-15 16:58:50 +0530

    Added seed skill

--- src/clients/mongo.client.ts
+++ src/clients/mongo.client.ts
@@ -4,9 +4,11 @@ import models from "../models/index";
 import { logger } from "../common/services/logger.service";
 import skills from "./skills.json" assert { type: "json" };
 import domains from "./domains.json" assert { type: "json" };
+import projectDomains from "./projectDomains.json" assert { type: "json" };
 
 import { SkillDAO } from "../dao/skills.dao";
 import { DomainDAO } from "../dao/domain.dao";
+import { ProjectDomainDAO } from "../dao/projectDomain.dao";
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace MongoClient {
@@ -28,9 +30,24 @@ export namespace MongoClient {
 
     logger.info("mongo client -> init -> dbModels", models);
 
-    // Seed skills and domains when its a new DB
+    // Seed skills, domains and project domains when its a new DB
+    seedProjectDomains();
     // await seedSkills();
     // await seedDomains();
+  }
+}
+
+export async function seedProjectDomains() {
+  const projectDomainDAO = new ProjectDomainDAO();
+  try {
+    const insertedProjectDomains =
+      await projectDomainDAO.addDomain(projectDomains);
+    logger.info(
+      `Successfully inserted ${insertedProjectDomains.length} project domains.`,
+    );
+  } catch (error: any) {
+    logger.error(`Failed to insert project domains: ${error.message}`);
+    // Handle error appropriately
   }
 }
 
@@ -51,7 +68,7 @@ export async function seedDomains() {
     const insertedDomains = await domainDAO.addDomain(domains);
     logger.info(`Successfully inserted ${insertedDomains.length} domains.`);
   } catch (error: any) {
-    logger.error(`Failed to insert skills: ${error.message}`);
+    logger.error(`Failed to insert domains: ${error.message}`);
     // Handle error appropriately
   }
 }
